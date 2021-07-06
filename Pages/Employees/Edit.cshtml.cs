5e67446be6f56eb10c2d0d51a4aab1d7fbe4b0ee
Guilherme Manteuffel <guilhermemb11@gmail.com>
2021-07-06 16:11:48 -0300

    edit page

--- Pages/Employees/Edit.cshtml.cs
+++ Pages/Employees/Edit.cshtml.cs
@@ -4,13 +4,30 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TutorialKudvenkat.Models;
+using TutorialKudvenkat.Services;
 
 namespace TutorialKudvenkat.Pages.Employees
 {
     public class EditModel : PageModel
     {
-        public void OnGet()
+        private readonly IEmployeeRepository employeeRepository;
+
+        public EditModel(IEmployeeRepository employeeRepository)
         {
+            this.employeeRepository = employeeRepository;
+        }
+
+        public Employee Employee { get; set; }
+
+        public IActionResult OnGet(int id)
+        {
+            Employee = employeeRepository.GetEmployee(id);
+            if (Employee == null)
+            {
+                RedirectToPage("/NotFound");
+            }
+            return Page();
         }
     }
 }
