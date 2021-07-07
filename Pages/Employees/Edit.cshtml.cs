1264fe6ca1f973905cfa84edbe16fa0ad376f601
Guilherme Manteuffel <guilhermemb11@gmail.com>
2021-07-07 09:17:03 -0300

    editar fotos

--- Pages/Employees/Edit.cshtml.cs
+++ Pages/Employees/Edit.cshtml.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using TutorialKudvenkat.Models;
@@ -12,13 +15,19 @@ namespace TutorialKudvenkat.Pages.Employees
     public class EditModel : PageModel
     {
         private readonly IEmployeeRepository employeeRepository;
+        private readonly IWebHostEnvironment webHostEnvironment;
 
-        public EditModel(IEmployeeRepository employeeRepository)
+        public EditModel(IEmployeeRepository employeeRepository,
+                         IWebHostEnvironment webHostEnvironment)
         {
             this.employeeRepository = employeeRepository;
+            this.webHostEnvironment = webHostEnvironment;
         }
 
         public Employee Employee { get; set; }
+        
+        [BindProperty]
+        public IFormFile Photo { get; set; }
 
         public IActionResult OnGet(int id)
         {
@@ -31,8 +40,40 @@ namespace TutorialKudvenkat.Pages.Employees
         }
         public IActionResult OnPost(Employee employee)
         {
+            if (Photo != null)
+            {
+                // If a new photo is uploaded, the existing photo must be
+                // deleted. So check if there is an existing photo and delete
+                if (employee.PhotoPath != null)
+                {
+                    string filePath = Path.Combine(webHostEnvironment.WebRootPath,
+                        "images", employee.PhotoPath);
+                    System.IO.File.Delete(filePath);
+                }
+                // Save the new photo in wwwroot/images folder and update
+                // PhotoPath property of the employee object
+                employee.PhotoPath = ProcessUploadedFile();
+            }   
             Employee = employeeRepository.Update(employee);
             return RedirectToPage("Index");
+        }
+
+        private string ProcessUploadedFile()
+        {
+            string uniqueFileName = null;
+
+            if (Photo != null)
+            {
+                string uploadsFolder = Path.Combine(webHostEnvironment.WebRootPath, "images");
+                uniqueFileName = Guid.NewGuid().ToString() + "_" + Photo.FileName;
+                string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    Photo.CopyTo(fileStream);
+                }
+            }
+
+            return uniqueFileName;
         }
     }
 }
