288bd34b2d46dd9ab074b45dbf575ee8188c0ef6
Guilherme Manteuffel <guilhermemb11@gmail.com>
2021-07-09 13:57:35 -0300

    viewcomponent

---
+++ Pages/ViewComponents/HeadCountViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TutorialKudvenkat.Services;
+
+namespace TutorialKudvenkat.Pages.ViewComponents
+{
+    public class HeadCountViewComponent : ViewComponent
+    {
+        private readonly IEmployeeRepository employeeRepository;
+
+        public HeadCountViewComponent(IEmployeeRepository employeeRepository)
+        {
+            this.employeeRepository = employeeRepository;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var result = employeeRepository.EmployeeCountByDept();
+            return View(result); 
+        }
+    }
+}
